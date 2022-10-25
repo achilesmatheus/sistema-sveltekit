934d8750702c1b81d8eceb68bf5e923d92114b1e
Achiles <achiles1707@gmail.com>
2022-10-24 22:44:44 -0300

    first commit

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { env } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 
 export const supabaseClient = createClient(
-	import.meta.env.PUBLIC_SUPABASE_URL ?? '',
-	import.meta.env.PUBLIC_SUPABASE_ANON_KEY ?? ''
+	env.PUBLIC_SUPABASE_URL ?? '',
+	env.PUBLIC_SUPABASE_ANON_KEY ?? ''
 );
