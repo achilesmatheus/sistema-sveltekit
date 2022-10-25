b5cbf03d25c6d6f9312d3e86897e32afe9e7014c
Achiles <achiles1707@gmail.com>
2022-10-24 23:22:20 -0300

    change: svelte config

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { env } from '$env/dynamic/public';
 
 export const supabaseClient = createClient(
-	'https://onwawyyanpyojbwqjnqt.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ud2F3eXlhbnB5b2pid3FqbnF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTExODEwNjUsImV4cCI6MTk2Njc1NzA2NX0.TflXXOV-7sHC6eLK09TVAnVz-jAaKlEJyAz6Uyzs0ls'
+	import.meta.env.VITE_SUPABASE_URL,
+	import.meta.env.VITE_SUPABASE_ANON_KEY
 );
